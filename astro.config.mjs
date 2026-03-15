2585c0c547fa24c157b5fb19e3ebefab78d15bab
andfelipeme <andfelipeme@gmail.com>
2026-03-15 17:34:57 -0500

    chore: update site URL to vhajdev.cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "static",
+  integrations: [sitemap()],
 });
